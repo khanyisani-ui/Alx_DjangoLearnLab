ae9dd054e3ea24b2a9bed37ea50e05460da917be
khanyisani-ui <khanyilekhanyisani8@gmail.com>
2024-11-09 19:47:27 +0200

    added roles

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -18,3 +18,12 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
 
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
+]
