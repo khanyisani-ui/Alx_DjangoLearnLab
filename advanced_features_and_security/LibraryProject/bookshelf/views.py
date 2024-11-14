2f90355387624ac42f63ebf27ec65ae7a6149323
khanyisani-ui <khanyilekhanyisani8@gmail.com>
2024-11-14 16:16:35 +0200

    fixed views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,38 +1,45 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required, permission_required
-from .models import Article
+from django.http import HttpResponseForbidden
+from .models import Book
+from .forms import BookForm # type: ignore
 
 @login_required
-@permission_required('yourapp.can_view', raise_exception=True)
-def article_list(request):
-    articles = Article.objects.all()
-    return render(request, 'yourapp/article_list.html', {'articles': articles})
+@permission_required('bookshelf.can_view_book', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
 
 @login_required
-@permission_required('yourapp.can_create', raise_exception=True)
-def article_create(request):
+@permission_required('bookshelf.can_add_book', raise_exception=True)
+def book_add(request):
     if request.method == 'POST':
-        # Handle article creation logic
-        pass
-    return render(request, 'yourapp/article_form.html')
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'bookshelf/book_form.html', {'form': form})
 
 @login_required
-@permission_required('yourapp.can_edit', raise_exception=True)
-def article_edit(request, pk):
-    article = get_object_or_404(Article, pk=pk)
+@permission_required('bookshelf.can_edit_book', raise_exception=True)
+def book_edit(request, pk):
+    book = get_object_or_404(Book, pk=pk)
     if request.method == 'POST':
-        # Handle article editing logic
-        pass
-    return render(request, 'yourapp/article_form.html', {'article': article})
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'bookshelf/book_form.html', {'form': form})
 
 @login_required
-@permission_required('yourapp.can_delete', raise_exception=True)
-def article_delete(request, pk):
-    article = get_object_or_404(Article, pk=pk)
+@permission_required('bookshelf.can_delete_book', raise_exception=True)
+def book_delete(request, pk):
+    book = get_object_or_404(Book, pk=pk)
     if request.method == 'POST':
-        article.delete()
-        return redirect('article_list')
-    return render(request, 'yourapp/article_confirm_delete.html', {'article': article})
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'bookshelf/book_confirm_delete.html', {'book': book})
