73ea641673564c5e07b856e5072972347a67f295
khanyisani-ui <khanyilekhanyisani8@gmail.com>
2024-11-15 10:46:06 +0200

    fixed https and redirect

--- advanced_features_and_security/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/settings.py
@@ -133,23 +133,3 @@ LOGOUT_REDIRECT_URL = '/'  # Redirect to homepage after logout
 # custom user model
 AUTH_USER_MODEL = 'bookshelf.CustomUser'
 
-# Redirect all HTTP requests to HTTPS
-SECURE_SSL_REDIRECT = True  # Ensure all connections are HTTPS.
-
-# HTTP Strict Transport Security (HSTS)
-SECURE_HSTS_SECONDS = 31536000  # Enforce HTTPS for 1 year.
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True  # Apply HSTS to all subdomains.
-SECURE_HSTS_PRELOAD = True  # Allow the domain to be included in the HSTS preload list.
-
-# Cookies
-SESSION_COOKIE_SECURE = True  # Ensure session cookies are sent over HTTPS.
-CSRF_COOKIE_SECURE = True  # Ensure CSRF cookies are sent over HTTPS.
-
-# Security Headers
-X_FRAME_OPTIONS = 'DENY'  # Prevent clickjacking.
-SECURE_CONTENT_TYPE_NOSNIFF = True  # Prevent MIME-sniffing.
-SECURE_BROWSER_XSS_FILTER = True  # Enable browser XSS filter.
-
-# Additional security settings
-SECURE_REFERRER_POLICY = 'no-referrer-when-downgrade'  # Referrer policy for security.
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')  # Handle reverse proxy setup.
