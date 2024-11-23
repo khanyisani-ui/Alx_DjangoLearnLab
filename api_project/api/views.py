628fe4eead39cd23183073329974bb619faaf391
khanyisani-ui <khanyilekhanyisani8@gmail.com>
2024-11-23 13:47:21 +0200

    added api endpoint

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from .models import Book, MyModel
+from .serializers import BookSerializer, MyModelSerializer
+
+class BookListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+    
+
