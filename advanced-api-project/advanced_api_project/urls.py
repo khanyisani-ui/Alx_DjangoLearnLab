74225470fc277f253a1763b6f6a2c72a42c48a32
khanyisani-ui <khanyilekhanyisani8@gmail.com>
2024-11-30 16:55:03 +0200

    fixed urls.py

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# advanced_api_project/urls.py
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # Include the urls from the 'api' app
+    path('api/', include('api.urls')),  # This connects the API views to the '/api/' path
 ]
+
