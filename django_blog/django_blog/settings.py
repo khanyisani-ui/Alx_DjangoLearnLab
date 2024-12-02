849d673201db2910bf1f2687dfbafe2851227c45
khanyisani-ui <khanyilekhanyisani8@gmail.com>
2024-12-02 15:41:24 +0200

    fixed database settings

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -78,7 +78,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'USER': 'khanyisanikhanyile',
+        'PASSWORD': 'Nkazimulo@Aya123',
+        'HOST':'127.0.0.1',
+        'PORT': '8000',
+        }
 }
 
 
